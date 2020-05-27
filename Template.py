b448fe27aff3925dfbd2d893201b1f100f136c85
perplexedcaleb <c.howard14@students.clark.edu>
2020-05-27 12:19:18 -0700

    Completed code

--- Template.py
+++ Template.py
@@ -12,6 +12,28 @@ Output: return value and description
 """
 
 def main():
-    # code here
+    # definite loop example
+    for i in range(0, 11, 2):
+        print(i)
+    print()
 
+    # same behavior but coded as indefinite or while loop
+    count = 0
+    while(count < 11):
+        print(count)
+        #count = count + 2
+        count += 2
+
+    x = True
+    y = True
+    if(x):
+        if(y):
+            print("both true")
+        else:
+            print("y false")
+    else:
+        print("x false")
+        
+    print()
+    
 main()    
